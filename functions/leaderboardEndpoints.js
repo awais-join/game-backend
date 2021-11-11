ef6cd65aff8c04a0ca3a5be9d9eae33b0018f8d5
themgi <the.mgi@outlook.com>
2021-11-12 04:22:12 +0500

    broken

--- functions/leaderboardEndpoints.js
+++ functions/leaderboardEndpoints.js
@@ -41,3 +41,17 @@ module.exports.postCurrentEntry = async (event) => {
     console.log('Exception occurred in postCurrentEntry in leaderboard.endpoint.js: ', error);
   }
 }
+
+module.exports.helloWorldCallback = async (event) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Headers": "*",
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+      "Accept": '*/*',
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({message: "hello world", description :"does this require authorization token yet? yes htis is working corec"})
+  }
+}
